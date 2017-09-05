db5df6d9ff29060fa8b0d14e176890778bf1e6e0
ArnaudM <arnaud.mathias@signall.com>
2017-09-05 22:03:57 +0200

    functionnal structure

---
+++ src/common/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => (
+  <div className="text-center">
+    <nav className="navbar navbar-default">
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="movies" activeClassName="active">Movies</Link>
+      {" | "}
+      <Link to="people" activeClassName="active">People</Link>
+      {" | "}
+      <Link to="species" activeClassName="active">Species</Link>
+      {" | "}
+      <Link to="locations" activeClassName="active">Locations</Link>
+      {" | "}
+      <Link to="terrain" activeClassName="active">Terrain</Link>
+    </nav>
+  </div>
+);
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+// Home page component. This serves as the welcome page with link to the library
+const HomePage = () => (
+  <div className="jumbotron center">
+    <h1 className="lead">Ghibli Explorer</h1>
+    <h4 className="subtitle">made with react</h4>
+    <div>
+      <Link to="library">
+        <button className="btn btn-lg btn-primary"> Start browsing movies</button>
+      </Link>
+    </div>
+  </div>
+);
+
+export default HomePage;
