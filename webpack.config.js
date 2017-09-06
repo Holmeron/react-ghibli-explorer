450f80baffdc3831d52bd9e1841e55c8406fe3f0
ArnaudM <arnaud.mathias@signall.com>
2017-09-06 21:20:28 +0200

    add bootstrap and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-    entry: "./src/index.js",
+    entry: ["./src/index.js"],
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.scss$/, loader: "sass-loader" },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
             {
               test: /\.js$/,
               exclude: /node_modules/,
@@ -17,6 +21,10 @@ module.exports = {
                   {
                     presets:['react','es2015']
                   }
+            },
+            {
+              test: /\.(eot|svg|ttf|woff|woff2)$/,
+              loader: "file-loader"
             }
         ]
     }
