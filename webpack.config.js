b33869a6a40f15150d9926215df8a5df85f000f6
ArnaudM <arnaud.mathias@signall.com>
2017-09-08 18:23:24 +0200

    webpack build ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: [
@@ -10,25 +11,25 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '',
-        filename: 'build.js'
+        publicPath: '/',
+        filename: 'bundle.js'
     },
-    context: path.resolve(__dirname, 'src'),
+    devServer: {
+      contentBase: 'dist/',
+      publicPath: '/'
+    },
     module: {
         rules: [
             {
-              test: /\.html$/,
-              use: 'file-loader'
-            },{
               test: /\.js$/,
               exclude: /node_modules/,
               use: [{
                 loader :'babel-loader',
                 query:{
-                      presets:['react','es2015','stage-2']
+                      presets:['react','es2015','stage-2'],
+                      plugins: ['transform-runtime']
                 }
-              }],
-
+              }]
             },{
                 test: /\.scss$/,
                 exclude: /node_modules/,
@@ -46,13 +47,18 @@ module.exports = {
                   publicPath: '../'
                 })
             },
-            {
-              test: /\.(eot|svg|ttf|woff|woff2)$/,
-              use: 'file-loader'
-            }
+            { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=images/[name].[ext]' },
+            { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader?&name=fonts/[name].[ext]' },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]' },
+            { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml&name=images/[name].[ext]' }
         ],
     },
     plugins: [
-      new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true })
+      new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
+      new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+      new CopyWebpackPlugin([
+            { from: './public/index.html', to: './' }
+      ])
     ]
 };
