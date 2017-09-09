57e237182bd6e8637a83d122678c7f6b5d1c6c28
ArnaudM <arnaud.mathias@signall.com>
2017-09-09 17:19:30 +0200

    clean useless stuff

--- src/actions/apiActions.js
+++ src/actions/apiActions.js
@@ -11,38 +11,3 @@ export const findAll = (entityType) => ({
   type: types.FIND_ALL,
   entityType
 });
-
-/** FILMS **/
-
-// find all films
-export const findAllFilms = () => ({
-  type: types.FIND_ALL_FILMS
-});
-// find film
-export const findFilm = (filmId) => ({
-  type: types.FIND_FILM,
-  filmId
-});
-// select a film
-export const selectFilmAction = (film) => ({
-  type: types.SELECTED_FILM,
-  film
-});
-
-
-// find all species
-export const findAllSpecies = () => ({
-  type: types.FIND_ALL_SPECIES
-});
-// find all locations
-export const findAllLocations = () => ({
-  type: types.FIND_ALL_LOCATIONS
-});
-// find all people
-export const findAllPeople = () => ({
-  type: types.FIND_ALL_PEOPLE
-});
-// find all vehicles
-export const findAllVehicles = () => ({
-  type: types.FIND_ALL_VEHICLES
-});
