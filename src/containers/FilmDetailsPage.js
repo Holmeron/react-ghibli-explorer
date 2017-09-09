bef07b2777ba218c9602523d0168101d13c2633c
ArnaudM <arnaud.mathias@signall.com>
2017-09-09 10:04:28 +0200

    update design

--- src/containers/FilmDetailsPage.js
+++ src/containers/FilmDetailsPage.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { findFilm } from '../actions/apiActions';
 
-class FilmsPage extends Component {
+import EntityLink from '../components/EntityLink/EntityLink';
+
+class FilmsDetailsPage extends Component {
 
  componentDidMount() {
    const {filmId} = this.props.params;
@@ -17,11 +19,34 @@ class FilmsPage extends Component {
         film ?
         <div className="helper__details-page film-details">
           <h2 className="helper__details-page_title"><i className="gb-icon info-popup"></i>{film.title}</h2>
-          <p className="helper__details-page_description"><i className="gb-icon document"></i>{film.description}</p>
+          <p className="helper__details-page_description">{film.description}</p>
           <p className="helper__details-page_comment"><i className="gb-icon comment"></i>Directed by : {film.director}</p>
           <p className="helper__details-page_comment flex-reverse"><i className="gb-icon reverse comment"></i>Produced by : {film.producer}</p>
           <p className="helper__details-page_date">Released : {film.release_date}</p>
           <p className="helper__details-page_score icons__rotten-tomato">Rottent Tomato score :{film.rt_score}</p>
+          <div className="helper__details-page_related">
+            {
+              film.people ?
+              films.people.map((peopleLink, index) =>(<EntityLink entityLink={film.people} />))
+              :
+              null
+            }{
+              film.species ?
+              films.species.map((speciesLink, index) =>(<EntityLink entityLink={film.species} />))
+              :
+              null
+            }{
+              film.locations ?
+              films.locations.map((locationsLink, index) =>(<EntityLink entityLink={film.locations} />))
+              :
+              null
+            }{
+              film.vehicles ?
+              films.vehicles.map((vehiclesLink, index) =>(<EntityLink entityLink={film.vehicles} />))
+              :
+              null
+            }
+          </div>
         </div>
         :
         null
@@ -36,4 +61,4 @@ const mapStateToProps = ({ films }) => ({
 
 // connect method from react-router connects the component with redux store
 export default connect(
-  mapStateToProps)(FilmsPage);
+  mapStateToProps)(FilmsDetailsPage);
