2ecda469c76d646d726081b1bdab95ce8636d8f4
ArnaudM <arnaud.mathias@signall.com>
2017-09-10 13:23:16 +0200

    added font, images and css

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router';
 
 // Home page component. This serves as the welcome page with link to the library
 const HomePage = () => (
-  <div className="jumbotron center">
+  <div className="home-page jumbotron center">
     <h1 className="lead">Ghibli Explorer</h1>
     <h4 className="subtitle">made with react</h4>
     <div>
       <Link to="films">
-        <button className="btn btn-lg btn-primary"> Start browsing movies</button>
+        <button className="btn btn-lg btn-primary"><i className="gb-icon approved"></i> Start browsing movies</button>
       </Link>
     </div>
   </div>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import Home from './components/Home';
+import Home from './containers/Home';
 
 import EntityDetailsPage from './containers/EntityDetailsPage';
 import ListPage from './containers/ListPage';
@@ -10,14 +10,19 @@ import ListPage from './containers/ListPage';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
+
     <Route path="films" component={ListPage} entityType='films' />
     <Route path="films/:entityId" component={EntityDetailsPage} entityType='films' />
+
     <Route path="people" component={ListPage} entityType='people' />
     <Route path="people/:entityId" component={EntityDetailsPage} entityType='people' />
+
     <Route path="species" component={ListPage} entityType='species' />
     <Route path="species/:entityId" component={EntityDetailsPage} entityType='species' />
+
     <Route path="locations" component={ListPage} entityType='locations' />
     <Route path="locations/:entityId" component={EntityDetailsPage} entityType='locations' />
+
     <Route path="vehicles" component={ListPage} entityType='vehicles' />
     <Route path="vehicles/:entityId" component={EntityDetailsPage} entityType='vehicles' />
   </Route>
